cb0713b5b7a2af9c606427f3e4334d85dec6d17e
nateusse <81440558+nateusse@users.noreply.github.com>
2021-09-10 19:29:44 -0500

    Add files via upload

---
+++ quiz_questions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 20:31:32 2021
+
+@author: nat
+
+user_input = input().split() 
+print(int(user_input[0]) + int(user_input[-1]))
+
+
+a = 2 
+b = 4 
+c = 8
+print(a, b, c, sep="*") 
+
+
+print(4 >> 1)
+
+if False: 
+    print("one", end=" ")
+    if True:
+        print("two",end=" ")
+    else: 
+        print("three", end=" ")
+else:
+    print("three", end=" ")
+print("four", end =" ")
+
+
+print(True and False and True or False)
+
+
+"""
+
+
